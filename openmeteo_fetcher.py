a69d6acc9c1a47827a905f19a9286832ff5255b1
darrenchilton <34659303+darrenchilton@users.noreply.github.com>
2025-12-04 20:21:53 -0500

    Update openmeteo_fetcher.py
    'forecast_days': 16

--- openmeteo_fetcher.py
+++ openmeteo_fetcher.py
@@ -59,7 +59,7 @@ class OpenMeteoFetcher:
                 'precipitation_sum,snowfall_sum,weather_code,wind_speed_10m_max'
             ),
             'timezone': 'America/New_York',
-            'forecast_days': 13
+            'forecast_days': 16
         }
 
         if start_time is not None:
